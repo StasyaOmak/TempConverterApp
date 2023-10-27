b3af31e478407a54c4e9b132bd1b74b5b5104c33
Anastasiya <ios.developer.by@gmail.com>
2023-10-27 11:00:48 +0300

    Add Logic

--- TempConverterApp/Controller/InfoViewController.swift
+++ TempConverterApp/Controller/InfoViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    var info: String?
+    var desc: String?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let info = info, let desc = desc {
+            infoLabel.text = info
+            descLabel.text = desc
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
--- TempConverterApp/Controller/ViewController.swift
+++ TempConverterApp/Controller/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var convertedTempLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var tempSlider: UISlider! {
+        didSet {
+            tempSlider.value = 0
+            tempSlider.minimumValue = 0
+            tempSlider.maximumValue = 100
+        }
+    }
+    @IBOutlet weak var tempSegmentControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        convertedTempLabel.text = "32 ºF"
     }
-
-
+    
+    @IBAction func sliderValueChange(_ sender: Any) {
+        updateTempLabelFromSlider(value: tempSlider.value)
+    }
+    
+    @IBAction func segmentValueChange(_ sender: Any) {
+        updateTempLabelFromSlider(value: tempSlider.value)
+    }
+    
+    
+#warning("pass result to Info VC without segue ")
+    
+    @IBAction func infoButtonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let infoScreen = storyboard.instantiateViewController(withIdentifier:
+        "InfoViewController") as! InfoViewController
+    
+        infoScreen.info = "Current Temperature"
+        infoScreen.desc = "\(celsiusLabel.text ?? "") or \(convertedTempLabel.text ?? "")"
+        
+        self.navigationController?.pushViewController(infoScreen, animated: true)
+    }
+    
+    
+    
+    func updateTempLabelFromSlider(value: Float) {
+        
+        let celsiusTemp = Int(tempSlider.value)
+        celsiusLabel.text = "\(celsiusTemp) ºC"
+        
+        let (fahrenheit, kelvin) = convertTempFrom(celsius: celsiusTemp)
+        let selectedSegmentIndex = tempSegmentControl.selectedSegmentIndex
+        
+        let temperatureText: String
+        if selectedSegmentIndex == 0 {
+            temperatureText = "\(fahrenheit) ºF"
+        } else {
+            temperatureText = "\(kelvin) ºK"
+        }
+        convertedTempLabel.text = temperatureText
+    }
+    
+    
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double) {
+        let tempFahrenheit = Double(celsius) * 9 / 5 + 32
+        let tempKelvins = Double(celsius) + 273.15
+        return (tempFahrenheit, tempKelvins)
+    }
 }
-
